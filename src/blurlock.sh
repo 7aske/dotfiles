82461fe53978a096a941c8c1fa85c895dd125371
Nikola Tasic <ntasic7@gmail.com>
2020-03-29 05:22:53 +0200

    blurlock

---
+++ src/blurlock.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# take screenshot
+import -window root /tmp/screenshot.png
+
+# blur it
+convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+rm /tmp/screenshot.png
+
+# lock the screen
+betterlockscreen -u /tmp/screenshotblur.png -l dimblur -t locked 
+sleep 1
+
+exit 0
