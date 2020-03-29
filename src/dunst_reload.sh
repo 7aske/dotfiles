7595a9cb49ab9abb2569031e985bfea7673a731c
Nikola Tasic <ntasic7@gmail.com>
2020-03-29 05:05:51 +0200

    dunst reload fix

--- src/dunst_reload.sh
+++ src/dunst_reload.sh
@@ -2,21 +2,18 @@
 
 . "${HOME}/.cache/wal/colors.sh"
 
-reload_dunst() {
-    killall dunst && dunst \
-        -frame_width 1 \
-        -lb "${color0}" \
-        -nb "${color0}" \
-        -cb "${color0}" \
-        -lfr "${color3}" \
-        -nfr "${color4}" \
-        -cfr "${color4}" \
-        -lf "${color3}" \
-        -cf "${color4}" \
-        -nf "${color4}" \
-        -bf "${color4}" \
-        -separator_color "${color4}" \
-        -conf ~/.config/dunst/dunstrc &
-}
-
-reload_dunst && notify-send "dunst" "dunst reloaded" 2>&1/dev/null
+killall dunst
+dunst \
+    -frame_width 1 \
+    -lb "${color0}" \
+    -nb "${color0}" \
+    -cb "${color0}" \
+    -lfr "${color3}" \
+    -nfr "${color4}" \
+    -cfr "${color4}" \
+    -lf "${color3}" \
+    -cf "${color4}" \
+    -nf "${color4}" \
+    -bf "${color4}" \
+    -separator_color "${color4}" \
+    -conf ~/.config/dunst/dunstrc &
