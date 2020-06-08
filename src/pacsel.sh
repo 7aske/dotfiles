4f19dff96f1b5ee676d4daed48d6636e4e153269
Nikola Tasic <ntasic7@gmail.com>
2020-06-08 12:11:56 +0200

    fixed pacsel opt

--- src/pacsel.sh
+++ src/pacsel.sh
@@ -10,7 +10,7 @@ function usage(){
 if [ $# -eq 1 ] || [ "$1" = "-" ]; then
 	package="$(yay -Ss "$1" | awk -e '$0 ~ /^[a-z]+/ {split($1, arr, "/"); print arr[2]}' | tac | dmenu -l 10)"
 elif [ $# -eq 0 ]; then
-	package="$(cat "-" | awk -e '$0 ~ /^[a-z]+/ {split($1, arr, "/"); print arr[2]}' |  tac | dmenu -n 10)"
+	package="$(cat "-" | awk -e '$0 ~ /^[a-z]+/ {split($1, arr, "/"); print arr[2]}' |  tac | dmenu -l 10)"
 else
 	usage
 fi
