49665a6e93e5b67d7c0577e53d703b14b603f4b7
Nikola Tasic <ntasic7@gmail.com>
2020-09-05 20:35:57 +0200

    rcode refactor using getopts

--- src/rcode.sh
+++ src/rcode.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env bash
 
-repo=$1
+function show_help (){
+	echo "usage "$(basename $0)" -?hpsv <repo>"
+	echo "    -?,h         show this message and exit"
+	echo "    -p <port>    ssh port used by rsync"
+	echo "    -s <host>    source host (default :\`hostname\`)"
+	echo "    -d <host>    destination host (default :\`hostname\`)"
+}
 
-declare src
-declare dest
+# A POSIX variable
+OPTIND=1
 
-if [ "$2" = "-s" ]; then
-	src="$3":"$CODE"/"$repo"
-	dest="$CODE"/"$repo"
-elif [ "$2" = "-d" ]; then
-	src="$CODE"/"$repo"
-	dest="$3":"$CODE"/"$repo"
+# Initialize our own variables:
+repo=""
+port=22
+dest=`hostname`
+src=`hostname`
+
+while getopts "h?:p:s:d:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    p)  port=$OPTARG
+        ;;
+    s)  src=$OPTARG
+        ;;
+    d)  dest=$OPTARG
+		echo $dest
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+dest="$dest:"
+src="$src:"
+
+if [ "$dest" != "$src" ]; then
+	rsync --progress -have "ssh -p $port" "$src$CODE/$1/" "$dest$CODE/$1/"
 else
-	echo "Invalid second argument."
+	show_help
 	exit 1
 fi
-if [ -z "$PORT" ]; then
-    rsync --progress -have ssh "$src" "$(dirname "$dest")"
-else
-    rsync --progress -have "ssh -p $PORT" "$src" "$(dirname "$dest")"
-fi
+
