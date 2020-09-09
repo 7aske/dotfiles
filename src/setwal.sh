a872a87a2ea95479adc79b9156d2a59d59066cf2
Nikola Tasic <ntasic7@gmail.com>
2020-09-09 17:41:11 +0200

    setwal - added screensaver generation, refactored to use getops

--- src/setwal.sh
+++ src/setwal.sh
@@ -2,6 +2,7 @@
 
 WALLPAPERS="${WALLPAPERS:-$HOME/Pictures/wallpaper}"
 DEFAULT_WALLPAPER="${DEFAULT_WALLPAPER:-$HOME/.config/wallpaper}"
+DEFAULT_SCREENSAVER="${DEFAULT_SCREENSAVER:-$HOME/.config/screensaver}"
 CMD="$(basename "$0")"
 
 [ -z "$(command -v wal)" ] && echo "wal: command not found" && exit 2
@@ -25,27 +26,35 @@ setwal_usage() {
 
 random_wallpaper() {
     PICTURE="$(find "$WALLPAPERS" -printf "$WALLPAPERS/%f\n" | shuf -n 1)"
-    set_wallpaper "$PICTURE"
+    set_wallpaper_wal "$PICTURE"
+	exit 0
 }
 
 reload_wallpaper() {
-    set_wallpaper "$DEFAULT_WALLPAPER"
+    set_wallpaper_wal "$DEFAULT_WALLPAPER"
+	exit 0
 }
 
 list_wallpapers() {
     PICTURE="$(find "$WALLPAPERS" -printf "$WALLPAPERS/%f\n" | sxiv -tio | sed '1q')"
     set_wallpaper "$PICTURE"
+	exit 0
 }
 
 set_wallpaper() {
-
     if echo "$1" | grep '^/' >/dev/null; then
         PICTURE="$1"
     else
         PICTURE="$(pwd)/$1"
     fi
+	[ -z "$PICTURE" ] && exit 1
     [ ! -L "$PICTURE" ] && ln -sf "$PICTURE" "$DEFAULT_WALLPAPER"
 	feh --bg-fill "$PICTURE"
+	set_screensaver &
+}
+
+set_screensaver() {
+	convert "$DEFAULT_WALLPAPER" -blur 0x60 "$DEFAULT_SCREENSAVER"
 }
 
 set_wallpaper_wal() {
@@ -54,22 +63,42 @@ set_wallpaper_wal() {
     else
         PICTURE="$(pwd)/$1"
     fi
+	[ -z "$PICTURE" ] && exit 1
     [ ! -L "$PICTURE" ] && ln -sf "$PICTURE" "$DEFAULT_WALLPAPER"
+	echo "$PICTURE"
     wal -c
-    wal --backend wal -i "$PICTURE" >/dev/null 2>&1 &
+    wal --backend wal -i "$PICTURE" &
+	set_screensaver &
 }
 
-if [ -z "$1" ]; then
+OPTIND=1
+wallpaper=""
+
+while getopts "h?rRlw:" opt; do
+    case "$opt" in
+    h|\?)
+        setwal_usage ;;
+    r)
+		reload_wallpaper ;;
+    R)
+		random_wallpaper ;;
+    w)
+		wallpaper=$OPTARG ;;
+	l)
+		list_wallpapers ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+
+if [ -n "$wallpaper" ]; then
+	set_wallpaper_wal "$wallpaper"
+elif [ -n "$1" ]; then
+	set_wallpaper "$1"
+else
     sxiv -t "$WALLPAPERS" &
-    exit 0
 fi
 
-case "$1" in
-"-h" | "--help") setwal_usage ;;
-"-r") reload_wallpaper ;;
-"-R") random_wallpaper ;;
-"-l") list_wallpapers ;;
-"-w") set_wallpaper_wal "$2" ;;
-*) set_wallpaper "$1" ;;
-esac || invalid_opt "$1"
-exit 0
