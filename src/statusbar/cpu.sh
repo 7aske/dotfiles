7ff3dfef3351833bb6dbe5e6b29df4b1e4dd0be2
Nikola Tasic <ntasic7@gmail.com>
2020-09-27 23:15:53 +0200

    cpu,weather - added rightclick popups

--- src/statusbar/cpu.sh
+++ src/statusbar/cpu.sh
@@ -2,6 +2,7 @@
 
 case $BLOCK_BUTTON in
 	1) notify-send "CPU hogs" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)" ;;
+	3) i3-msg "exec --no-startup-id setsid -f st -c floating_popup -e htop" 2>&1>/dev/null ;;
 esac
 
 mpstat 1 1 | grep 'Average:' | awk '{printf "%d%\n", $3}'
--- src/statusbar/weather.sh
+++ src/statusbar/weather.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
+
+function show_weather(){
+	#i3-msg "workspace weather"
+	curl -s http://wttr.in > /tmp/weather 2>&1
+	i3-msg "exec --no-startup-id setsid -f st -c weather_floating -e less -Srf /tmp/weather" 2>&1>/dev/null
+}
+
 case $BLOCK_BUTTON in
 	1) notify-send "Weather" "$(curl wttr.in/ | perl -pe 's/\e\[[0-9;]*m(?:\e\[K)?//g' | head -7)" ;;
+	3) show_weather ;;
 esac
 
 echo "$(curl wttr.in/?format=%t)"
