ae17923eac2bf5c569d3cdf1a942497ebc789b64
Nikola Tasic <ntasic7@gmail.com>
2020-10-05 00:04:16 +0200

    dunst - wal check

--- src/dunst_reload.sh
+++ src/dunst_reload.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
-[ -f  "${HOME}/.cache/wal/colors.sh" ] && . "${HOME}/.cache/wal/colors.sh"
-
 killall dunst
-dunst \
-    -frame_width 1 \
-    -lb "${color0}" \
-    -nb "${color0}" \
-    -cb "${color0}" \
-    -lfr "${color3}" \
-    -nfr "${color4}" \
-    -cfr "${color4}" \
-    -lf "${color3}" \
-    -cf "${color4}" \
-    -nf "${color4}" \
-    -bf "${color4}" \
-    -separator_color "${color4}" \
-    -conf ~/.config/dunst/dunstrc &
+if [ -f  "${HOME}/.cache/wal/colors.sh" ]; then
+    . "${HOME}/.cache/wal/colors.sh"
+    dunst \
+        -frame_width 1 \
+        -lb "${color0}" \
+        -nb "${color0}" \
+        -cb "${color0}" \
+        -lfr "${color3}" \
+        -nfr "${color4}" \
+        -cfr "${color4}" \
+        -lf "${color3}" \
+        -cf "${color4}" \
+        -nf "${color4}" \
+        -bf "${color4}" \
+        -separator_color "${color4}" \
+        -conf ~/.config/dunst/dunstrc &
+else
+    dunst \
+        -frame_width 1 \
+        -conf ~/.config/dunst/dunstrc &
+fi
+
