9bc5766ae26be6cc83da4a27066e666c7b90d530
Nikola Tasic <ntasic7@gmail.com>
2020-10-05 00:50:03 +0200

    xpick - no copy if no color

--- src/xpick.sh
+++ src/xpick.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 color="$(colorpicker --short --one-shot --preview)"
-
-echo -n "$color" | xclip -sel c
-notify-send --hint=int:transient:1 -t 5000  "xpick" "'$color' copied to clipboard"
+if [ -n "$color" ]; then
+	echo -n "$color" | xclip -sel c
+	notify-send --hint=int:transient:1 -t 5000  "xpick" "'$color' copied to clipboard"
+fi
