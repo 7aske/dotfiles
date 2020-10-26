3735bde95a0b0f0f207d96d087d6f525223cad80
Nikola Tasic <ntasic7@gmail.com>
2020-10-26 23:11:17 +0100

    npile - pandoc md compiler

--- src/npile.sh
+++ src/npile.sh
@@ -3,16 +3,34 @@
 file=$(readlink -f "$1")
 dir=$(dirname "$file")
 base="${file%.*}"
+MD_COMPLIER="${MD_COMPILER:-"pandoc"}"
+
+function _compile_md_pandoc(){
+	pandoc \
+		--pdf-engine=xelatex \
+        -V 'mainfont:NotoSerif-Regular' \
+        -V 'sansfont:NotoSans-Regular' \
+        -V 'monofont:FiraCode-Regular' \
+		-o "$base.pdf" \
+		"$base.md"
+	(pgrep -fi  "$base.pdf" 2>&1>/dev/null) || (zathura "$base.pdf" &)
+}
+
+function _compile_md_r(){
+	Rscript -e "rmarkdown::render('$file', quiet=TRUE)"
+	pdf_filename="$base.pdf"
+	html_filename="$base.html"
+	QT_STYLE_OVERRIDE='Windows' wkhtmltopdf "$html_filename" "$pdf_filename"
+	rm "$html_filename"
+	(pgrep -fi  "$pdf_filename" 2>&1>/dev/null) || (zathura "$pdf_filename" &)
+}
 
 case "$file" in
 	*\.md)
-		Rscript -e "rmarkdown::render('$file', quiet=TRUE)"
-		pdf_filename="$base.pdf"
-		html_filename="$base.html"
-		QT_STYLE_OVERRIDE='Windows' wkhtmltopdf "$html_filename" "$pdf_filename"
-		rm "$html_filename"
-		(pgrep -fi  "$pdf_filename" 2>&1>/dev/null) || (zathura "$pdf_filename" &)
-		;;
+		case "$MD_COMPLIER" in
+			"pandoc")  _compile_md_pandoc ;;
+			"r")       _compile_md_r ;;
+		esac ;;
 	*config.h) sudo make install ;;
 	*\.c) cc "$file" -o "$base" && "$base" ;;
 	*\.py) python "$file" ;;
