c7dfb470cef1a80ecad7d087d364f5d8e6ddf772
Nikola Tasic <ntasic7@gmail.com>
2020-10-28 12:54:43 +0100

    homeclean - for loop and verbose

--- src/homeclean.sh
+++ src/homeclean.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env sh
 
+junk=(
+	"$HOME/.node_repl_history"
+	"$HOME/.python_history"
+	"$HOME/.xsession_errors*"
+	"$HOME/.viminfo"
+	"$HOME/.mysql_history"
+	"$HOME/.sudo_as_admin_successful"
+	"$HOME/.mongorc.json"
+	"$HOME/.lesshst"
+	"$HOME/.yarnrc"
+	"$HOME/.dir_colors"
+	"$HOME/.dbshell"
+	"$HOME/.NERDTreeBookmarks"
+	"$HOME/.angular-config.json"
+	"$HOME/.install4j"
+	"$HOME/.wget-hsts"
+)
 
-rm ~/.node_repl_history
-rm ~/.python_history
-rm ~/.xsession_errors*
-rm ~/.viminfo
-rm ~/.mysql_history
-rm ~/.sudo_as_admin_successful
-rm ~/.mongorc.json
-rm ~/.lesshst
-rm ~/.yarnrc
-rm ~/.dir_colors
-rm ~/.dbshell
-rm ~/.NERDTreeBookmarks
-rm ~/.angular-config.json
-rm ~/.install4j
-rm ~/.wget-hsts
+for j in ${junk[@]}; do
+	if [ -n "$j" ] && [ -e "$j" ]; then
+		rm -v "$j"
+	fi
+done
+
 
