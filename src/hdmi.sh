fd68c33fe97978c1921ef62d311c3aa8d5d5ecff
Nikola Tasic <ntasic7@gmail.com>
2020-11-04 12:07:17 +0100

    hdmi - smarter screen selection

--- src/hdmi.sh
+++ src/hdmi.sh
@@ -3,10 +3,10 @@
 MODE=${2:-"same-as"}
 
 PRIMARY="$(xrandr | grep primary | cut -d' ' -f1)"
-DISPL="${DISPL:-"HDMI-1"}"
+MONITOR="$(xrandr --current | sed -n -e '/connected [^primary]/p' | cut -d ' ' -f1 | head -1)"
 
 if [ "$1" = "on" ]; then
-    xrandr --output "$PRIMARY" --auto --output "$DISPL" --auto "--$MODE" "$PRIMARY"
+    xrandr --output "$MONITOR" --auto "--$MODE" "$PRIMARY"
 elif [ "$1" = "off" ]; then
-    xrandr --output "$DISPL" --off
+    xrandr --output "$MONITOR" --off
 fi
