c96620f167bfd828590fce4368d3ba48084a2303
Nikola Tasic <ntasic7@gmail.com>
2020-11-15 19:00:48 +0100

    statusbar/cpu - fixed shell syntax

--- src/statusbar/cpu.sh
+++ src/statusbar/cpu.sh
@@ -2,7 +2,7 @@
 
 case $BLOCK_BUTTON in
 	1) notify-send -i cpu "CPU hogs" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)" ;;
-	3) i3-msg "exec --no-startup-id setsid -f st -c floating_popup -e htop" 2>&1>/dev/null ;;
+	3) i3-msg "exec --no-startup-id setsid -f st -c floating_popup -e htop" 2>/dev/null 1>/dev/null ;;
 esac
 
 mpstat 1 1 | grep 'Average:' | awk '{printf "%d%\n", $3}'
