889b65c1f576c369f3c5b544255f26cc0640ebf7
Nikola Tasic <ntasic7@gmail.com>
2020-11-21 18:06:41 +0100

    vimcfg - added check whether the scripts is running in the term or not

--- src/vimcfg.sh
+++ src/vimcfg.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 prog="$(basename $0)"
-find_flags=" -maxdepth 3 -type f "
-find_cmd="find "
+find_flags="-maxdepth 3 -type f"
+find_cmd="find"
 case "$1" in
     "--etc")  find_cmd="sudo find "; cfg_dir="/etc" ;;
     "--home") find_flags=" -maxdepth 1 -type f"; cfg_dir="$HOME" ;;
@@ -11,15 +11,19 @@ esac
 
 [ -z "$EDITOR" ] &&  echo "$prog: EDITOR env variable not set" && exit 1
 [ ! -d "$cfg_dir" ] && echo "$prog: $cfg_dir: no such file or directory" && exit 1
-files="$(eval $find_cmd $cfg_dir $find_flags)"
-config_file="$(echo $files | sed 's/\ /\n/g' | grep -v ".git" | fzf --reverse --cycle)"
+files="$($find_cmd $cfg_dir $find_flags | sort)"
+
+config_file="$(echo $files | sed 's/\ /\n/g' | grep -v ".git" | dmenu -fn 'Fira Code Medium-10' -f -i -l 10 )"
+
+cmd="$EDITOR $config_file"
+[ ! -w "$config_file" ] && cmd="sudo $EDITOR $config_file"
 
 if [ -f "$config_file" ]; then
-    if [ -w "$config_file" ]; then
-        eval "$EDITOR $config_file"
-    else
-        eval "sudo $EDITOR $config_file"
-    fi
+	if [ ! -t 1 ]; then
+		$TERMINAL -e $cmd
+	else
+		$cmd
+	fi
 elif [ -n "$config_file" ]; then
     echo "$prog: $config_file: no such file or directory" && exit 1
 fi
