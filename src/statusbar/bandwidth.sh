fb1ca8c029dbaab0e6a3d209d097613d16c17af5
Nikola Tasic <ntasic7@gmail.com>
2020-12-15 19:08:30 +0100

    statusbar/bandwidth - shows Mib/kib/b

--- src/statusbar/bandwidth.sh
+++ src/statusbar/bandwidth.sh
@@ -1,26 +1,50 @@
 #!/usr/bin/env sh
 
+_bc() {
+	echo "scale=${2:-"2"}; $1" | bc
+}
+
 case $BLOCK_BUTTON in
     1) notify-send -i modem "Public IP" "$(curl -s api.ipify.org)" ;;
     3) notify-send -i network-wired "Local IP" "$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -1)" ;;
 esac
 
+IFACE="$(ip link | grep -e "BROADCAST" | sed 1q | awk '{print $2}' | cut -d ':' -f1)"
 
+SLEEP=1
 
+TIME_START=$(echo '('`date +"%s.%N"` ' * 1000000)/1' | bc)
 
-iface="$(ip link | grep -e "BROADCAST" | sed 1q | awk '{print $2}' | cut -d ':' -f1)"
+RX_OLD="$(cat /sys/class/net/"$IFACE"/statistics/rx_bytes)"
+TX_OLD="$(cat /sys/class/net/"$IFACE"/statistics/tx_bytes)"
 
+sleep $SLEEP
 
-SLP=2
-RX="$(cat /sys/class/net/"$iface"/statistics/rx_bytes)"
-TX="$(cat /sys/class/net/"$iface"/statistics/tx_bytes)"
+RX_NEW="$(cat /sys/class/net/"$IFACE"/statistics/rx_bytes)"
+TX_NEW="$(cat /sys/class/net/"$IFACE"/statistics/tx_bytes)"
 
-sleep $SLP
+RX_DELTA=$((RX_NEW - RX_OLD))
+TX_DELTA=$((TX_NEW - TX_OLD))
 
-RX2="$(cat /sys/class/net/"$iface"/statistics/rx_bytes)"
-TX2="$(cat /sys/class/net/"$iface"/statistics/tx_bytes)"
+TIME_END=$(echo '('`date +"%s.%N"` ' * 1000000)/1' | bc)
 
-RRX="$(((RX2 - RX) / 1000 / SLP))"
-RTX="$(((TX2 - TX) / 1000 / SLP))"
+TIME_DELTA="$(_bc "$((TIME_END - TIME_START)) / 1000000")"
 
-echo " $(printf '%5dk' $RRX)" "祝$(printf '%5dk' $RTX)"
+if [ $RX_DELTA -gt 1048576 ]; then
+	RX_OUT="$(_bc "$RX_DELTA / 1048576 / $TIME_DELTA")M"
+elif [ $RX_DELTA -gt 1024 ]; then
+	RX_OUT="$(_bc "$RX_DELTA / 1024 / $TIME_DELTA" 0)k"
+else
+	RX_OUT="$(_bc "$RX_DELTA / $TIME_DELTA" 0)"
+fi
+
+if [ $TX_DELTA -gt 1048576 ]; then
+	TX_OUT="$(_bc "$TX_DELTA / 1048576 / $TIME_DELTA")M"
+elif [ $TX_DELTA -gt 1024 ]; then
+	TX_OUT="$(_bc "$TX_DELTA / 1024 / $TIME_DELTA" 0)k"
+else
+	TX_OUT="$(_bc "$TX_DELTA / $TIME_DELTA" 0)"
+
+fi
+
+echo " $(printf '%5s' $RX_OUT)" "祝$(printf '%5s' $TX_OUT)"
