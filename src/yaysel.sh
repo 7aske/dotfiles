dd2c74cc71a729c6ed7de3f109bf77801bff8025
Nikola Tasic <ntasic7@gmail.com>
2020-12-31 03:46:25 +0100

    newsboat, yaysel

---
+++ src/yaysel.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+prog="$(basename $0)"
+
+function usage(){
+	echo "usage: $prog [query]" 1>&2
+	exit 1
+}
+
+if [ $# -eq 1 ] || [ "$1" = "-" ]; then
+	package="$(yay -Ss "$1" | awk -e '$0 ~ /^[a-z]+/ {split($1, arr, "/"); print arr[2]}' | tac | fzf)"
+elif [ $# -eq 0 ]; then
+	package="$(cat "-" | awk -e '$0 ~ /^[a-z]+/ {split($1, arr, "/"); print arr[2]}' |  tac | fzf)"
+else
+	usage
+fi
+
+
+if [ -n "$package" ]; then
+	yes "" | yay -S "$package"
+fi
