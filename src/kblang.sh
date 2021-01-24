03790f141a62d91f604a6ac1bca5d3a12ad1d3c0
Nikola Tasic <ntasic7@gmail.com>
2021-01-24 15:28:55 +0100

    kblang - quick variants

--- src/kblang.sh
+++ src/kblang.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 quick_layouts="us\nrs"
+declare -A variants
+variants=( ["rs"]="latin\n " ["us"]=" ")
 
 layout="$(cat <(echo -e $quick_layouts) <(localectl list-x11-keymap-layouts --no-pager) | dmenu -fn 'Fira Code-10' -p "layout:")"
 
 if [ -n "$layout" ]; then
-    variant="$(cat <(echo -e " ") <(localectl list-x11-keymap-variants "$layout") | dmenu -fn 'Fira Code-10' -p "$layout:")"
+    variant="$(cat <(echo -e "${variants[$layout]}") <(localectl list-x11-keymap-variants "$layout") | dmenu -fn 'Fira Code-10' -p "$layout:")"
     if [ -n "$variant" ] && [ "$variant" != " " ]; then
         setxkbmap -layout "$layout" -variant "$variant"
     else
