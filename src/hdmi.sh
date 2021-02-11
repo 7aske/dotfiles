547a06d72af9e5485960b54ab9ba59208568c960
Nikola Tasic <ntasic7@gmail.com>
2021-02-11 22:12:35 +0100

    hdmi - mode fix

--- src/hdmi.sh
+++ src/hdmi.sh
@@ -6,7 +6,8 @@ PRIMARY="$(xrandr | grep primary | cut -d' ' -f1)"
 MONITOR="$(xrandr --current | sed -n -e '/connected [^primary]/p' | cut -d ' ' -f1 | head -1)"
 
 if [ "$1" = "on" ]; then
-    xrandr --output "$MONITOR" --auto "--$MODE" "$PRIMARY"
+    xrandr --output "$MONITOR" --mode "1920x1080" "--$MODE" "$PRIMARY"
+	xrandr --output "$PRIMARY" --mode "1920x1080" --primary
 elif [ "$1" = "off" ]; then
     xrandr --output "$MONITOR" --off
 fi
