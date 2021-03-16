916b1b999b4b1c758f89c219fb284d8a15cb9dff
Nikola Tasic <ntasic7@gmail.com>
2021-03-16 09:15:34 +0100

    statusbar - ram abs numbers

--- src/statusbar/memory.sh
+++ src/statusbar/memory.sh
@@ -4,5 +4,5 @@ case $BLOCK_BUTTON in
 	1) notify-send -i device_mem "Memory hogs" "$(ps axch -o cmd:15,%mem --sort=-%mem | head)" ;;
 esac
 
-#free -h | awk '/^Mem:/ {print $3 "/" $2}'
-free | awk '/^Mem:/ {printf "%d%\n", (($3 / $2 * 100 ))}'
+free --mega -h | awk '/^Mem:/ {print $3 "/" $2}'
+#free | awk '/^Mem:/ {printf "%d%\n", (($3 / $2 * 100 ))}'
