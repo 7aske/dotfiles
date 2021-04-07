fd00a320dfa69796b26c7fb71865f3912191bbd6
Nikola Tasic <ntasic7@gmail.com>
2021-04-07 10:26:29 +0200

    hdmi - handle mariner tv

--- src/hdmi.sh
+++ src/hdmi.sh
@@ -2,12 +2,35 @@
 
 MODE=${2:-"same-as"}
 
-PRIMARY="$(xrandr | grep primary | cut -d' ' -f1)"
-MONITOR="$(xrandr --current | sed -n -e '/connected [^primary]/p' | cut -d ' ' -f1 | head -1)"
 
-if [ "$1" = "on" ]; then
-    xrandr --output "$MONITOR" --mode "1920x1080" "--$MODE" "$PRIMARY"
-	xrandr --output "$PRIMARY" --mode "1920x1080" --primary
-elif [ "$1" = "off" ]; then
-    xrandr --output "$MONITOR" --off
-fi
+_usage() {
+	echo "$0: host $(hostname) not supported"
+	exit 2
+}
+
+_handle_mariner() {
+	MIRROR="${MIRROR:-"HDMI-2"}"
+	TV="${TV:-"HDMI-1"}"
+	if [ "$1" = "on" ]; then
+		xrandr --output "$TV" --auto --same-as "$MIRROR"
+	elif [ "$1" = "off" ]; then
+		xrandr --output "$TV" --off
+	fi
+}
+
+_handle_juno() {
+	PRIMARY="$(xrandr | grep primary | cut -d' ' -f1)"
+	MONITOR="$(xrandr --current | sed -n -e '/connected [^primary]/p' | cut -d ' ' -f1 | head -1)"
+	if [ "$1" = "on" ]; then
+		xrandr --output "$MONITOR" --mode auto "--$MODE" "$PRIMARY"
+		xrandr --output "$PRIMARY" --mode "1920x1080" --primary
+	elif [ "$1" = "off" ]; then
+		xrandr --output "$MONITOR" --off
+	fi
+}
+
+case $(hostname) in
+	juno) _handle_juno $@ ;;
+	mariner) _handle_mariner $@ ;;
+	*) 
+esac 
