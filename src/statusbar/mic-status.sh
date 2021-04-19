1f9e5530bdad631cb9c6ecadbe51e80762e0626f
Nikola Tasic <ntasic7@gmail.com>
2021-04-19 03:42:37 +0200

    statusbar/mic-status - fixed icon

--- src/statusbar/mic-status.sh
+++ src/statusbar/mic-status.sh
@@ -4,10 +4,16 @@
 
 inputs="$(pactl list source-outputs | grep application.process.binary)"
 
+case $BLOCK_BUTTON in
+	1) notify-send -i audio-recorder "toggled inputs" && patoggle sources ;;
+	2) pavucontrol ;;
+	3) notify-send -i audio-recorder "active inputs" "$(echo $inputs | cut -d= -f2 | sed -e 's/["\ ]//g')" ;;
+esac
+
 color="${color2:-"#A3BE8C"}"
 muted_color="${color1:-"#BF616A"}"
-icon=" "
-muted_icon=" "
+icon=""
+muted_icon=""
 
 sources="$(pactl list sources short | wc -l)"
 muted="$(pactl list sources | grep "Mute: yes" | wc -l)"
@@ -17,11 +23,6 @@ if [ $sources -eq $muted ] && [ $muted -gt 0 ]; then
 	icon="$muted_icon"
 fi
 
-case $BLOCK_BUTTON in
-	1) notify-send -i audio-recorder "toggled inputs" && patoggle sources ;;
-	2) pavucontrol ;;
-	3) notify-send -i audio-recorder "active inputs" "$(echo $inputs | cut -d= -f2 | sed -e 's/["\ ]//g')" ;;
-esac
 
 
 if [ -n "$inputs" ]; then
