c6bd09ff104cacad8b04f4a2b22a433d30a79960
Nikola Tasic <ntasic7@gmail.com>
2021-05-03 15:42:34 +0200

    statusbar/services - added port check for mysql/postgres

--- src/statusbar/services.sh
+++ src/statusbar/services.sh
@@ -3,16 +3,19 @@ if [ "$(systemctl is-active docker)" = "active" ]; then
 	SERVICES="$SERVICES "
 fi
 
-if [ "$(systemctl is-active mysqld)" = "active" ] || [ "$(systemctl is-active mariadb)" = "active" ]; then
+if [ "$(systemctl is-active mysqld)" = "active" ] \
+	|| [ "$(systemctl is-active mariadb)" = "active" ] \
+	|| [ "$(netstat -tln | grep -cE "(127.0.0.1|::1):3306")" -gt 0 ]; then
 	SERVICES="$SERVICES  "
 fi
 
-if [ "$(systemctl is-active postgresql)" = "active" ]; then
+if [ "$(systemctl is-active postgresql)" = "active" ] \
+	|| [ "$(netstat -tln | grep -cE "(127.0.0.1|::1):5432")" -gt 0 ]; then
 	SERVICES="$SERVICES  "
 fi
 
 if [ "$(systemctl is-active nginx)" = "active" ]; then
-	SERVICES="$SERVICES "
+	SERVICES="$SERVICES  "
 fi
 
 if [ -z "$SERVICES" ]; then
