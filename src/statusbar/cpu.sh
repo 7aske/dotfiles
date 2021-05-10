8a0b6efc169d888ad0be479f384c4caa8777dea3
Nikola Tasic <ntasic7@gmail.com>
2021-05-10 22:07:20 +0200

    cpu - usage colors

--- src/statusbar/cpu.sh
+++ src/statusbar/cpu.sh
@@ -5,4 +5,17 @@ case $BLOCK_BUTTON in
 	3) i3-msg "exec --no-startup-id setsid -f st -c floating_popup -e htop" 2>/dev/null 1>/dev/null ;;
 esac
 
-mpstat 1 1 | grep 'Average:' | awk '{printf "%d%\n", ((100 - $12))}'
+cpu_usage="$(mpstat 1 1 | grep 'Average:' | awk '{printf "%d", ((100 - $12))}')"
+
+
+if [ "$cpu_usage" -ge 90 ]; then
+	color="${color1:-"#BF616A"}"
+elif [ "$cpu_usage" -ge 75 ]; then
+	color="${color3:-"#D08770"}"
+elif [ "$cpu_usage" -ge 50 ]; then
+    color="${color2:-"#EBCB8B"}"
+else
+	color="${color7:-"#D8DEE9"}"
+fi
+
+printf "<span color='%s'>%d%%</span>\n" "$color" "$cpu_usage"
