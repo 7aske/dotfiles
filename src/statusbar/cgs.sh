b77acab20871293c949c40ada51a453f0fa17c00
Nikola Tasic <ntasic7@gmail.com>
2021-06-15 16:04:55 +0200

    statusbar/cgs - added colors

--- src/statusbar/cgs.sh
+++ src/statusbar/cgs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 [ -z "$CODE" ] && return 1
+[ -f  "$HOME/.config/colors.sh" ] && . "$HOME/.config/colors.sh"
+[ -f  "$HOME/.cache/wal/colors.sh" ] && . "$HOME/.cache/wal/colors.sh"
 
 case $BLOCK_BUTTON in
     1) notify-send -i git "Repositories" "$(cgs)" ;;
@@ -9,10 +11,20 @@ esac
 
 repos="$(/usr/bin/cgs | wc -l)"
 
+if [ "$repos" -le 1 ]; then
+	color="${color7:-"#ffffff"}"
+elif [ "$repos" -le 3 ]; then
+    color="${color3:-"#fef44e"}"
+elif [ "$repos" -le 5 ]; then
+	color="${color5:-"#ff5252"}"
+else
+	color="${color1:-"#ff8144"}"
+fi
+
 if [ $repos -eq 0 ]; then
 	exit 0
 fi
 
-echo "$repos"
+echo "<span color='$color'>$repos</span>"
 
 
