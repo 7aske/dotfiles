e426686a907323f8c9071102fbbdc62997399082
Nikola Tasic <ntasic7@gmail.com>
2021-06-27 01:54:41 +0200

    codeclean - revamp

--- src/codeclean.sh
+++ src/codeclean.sh
@@ -2,15 +2,23 @@
 
 [ -z "$CODE" ] && echo "'CODE' not set" && exit 0
 
-find "$CODE" -type d \(\
-	-name out -prune -or\
-	-name target -prune -or\
-	-name dist -prune -or\
-	-name cmake-build-release -prune -or\
-	-name cmake-build-debug -prune -or\
-	-name node_modules -prune -or\
-	-name build -prune -or\
-	-name __pycache__ -prune\
-	\) -exec rm -rf {} \;
+FOLDERS="$(find "$CODE" -type d \(\
+	-name out                 -prune -o\
+	-name target              -prune -o\
+	-name dist                -prune -o\
+	-name cmake-build-release -prune -o\
+	-name cmake-build-debug   -prune -o\
+	-name node_modules        -prune -o\
+	-name __pycache__         -prune -o\
+	-name build               -prune -a\
+	\! -path \*neovim\*       -prune   \
+	\) -prune -printf "%p\n")"
+
+for F in $FOLDERS; do 
+	echo $F
+	if [ -w "$F" ] && ( [ -O "$F" ] || [ -G "$F" ] ); then
+		rm -rI "$F"
+	fi
+done
 
 
