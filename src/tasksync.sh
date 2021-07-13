bc84e8e9aa5a508b818e968330957f2424aaea36
Nikola Tasic <ntasic7@gmail.com>
2021-07-13 21:01:15 +0200

    added tasks and tasksync

---
+++ src/tasksync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash 
+
+_sync() {
+	[ -z "$1" ] && return 1
+	[ -z "$2" ] && return 1
+	([ ! -e "$1" ] && [ ! -e "$2" ]) && return 1
+	rsync -auz "$1" "$2"
+}
+
+TASKSERVER="${TASKSEVER:-"7aske.com"}"
+TASKSERVER_USER="${TASKSEVER_USER:-"nik"}"
+
+SERVER_TASKDATA="$(ssh $TASKSERVER ". \$HOME/.profile; echo \$TASKDATA")"
+
+[ -z "$SERVER_TASKDATA" ] && exit 1
+[ -z "$TASKDATA" ]        && exit 1
+[ -z "$TASKSERVER" ]      && exit 1
+
+SERVER_FOLDER="$TASKSERVER_USER@$TASKSERVER:$SERVER_TASKDATA/"
+LOCAL_FOLDER="$TASKDATA"
+
+echo "$TASKSERVER -> $(hostname)"
+_sync "$SERVER_FOLDER" "$LOCAL_FOLDER" || exit 2
+
+echo "$TASKSERVER <- $(hostname)"
+_sync "$LOCAL_FOLDER" "$SERVER_FOLDER" 
