1e35de1d37009153a64a438792b133f148efd756
Nikola Tasic <ntasic7@gmail.com>
2021-07-25 00:24:42 +0200

    mic-status - switched to using padefault

--- src/statusbar/mic-status.sh
+++ src/statusbar/mic-status.sh
@@ -5,7 +5,7 @@
 inputs="$(pactl list source-outputs | grep application.process.binary)"
 
 case $BLOCK_BUTTON in
-	1) notify-send -i audio-recorder "toggled inputs" && patoggle sources ;;
+	1) notify-send -i audio-recorder "toggled inputs" && padefault mute-all-src ;;
 	2) pavucontrol ;;
 	3) notify-send -i audio-recorder "active inputs" "$(echo $inputs | cut -d= -f2 | sed -e 's/["\ ]//g')" ;;
 esac
