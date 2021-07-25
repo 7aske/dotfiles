bcbf2894cf3c6220e9af6374b26704737e774c26
Nikola Tasic <ntasic7@gmail.com>
2021-07-26 00:41:23 +0200

    statusbar/music - refactored

--- src/statusbar/music.sh
+++ src/statusbar/music.sh
@@ -2,16 +2,12 @@
 
 . "$HOME/.profile"
 
-if [ -n "$1" ]; then
-    PLAYER="$1"
-elif [ -z "$PLAYER" ]; then
-    # default player
-    PLAYER="spotify"
+if [ "$(playerctl "--player=$PLAYER" status 2>&1)" = "No players found" ]; then
+	PLAYER="$(playerctl --list-all | cut -d'.' -f1 | head -1)"
 fi
 
-PLAYER_ARG="--player=$PLAYER,%any,chromium"
+PLAYER_ARG="--player=$PLAYER,%any"
 PLAYER_STATUS="$(playerctl "$PLAYER_ARG" status 2>&1)"
-
 if [ "$PLAYER_STATUS" = "No players found" ] && [ -n "$BLOCK_BUTTON" ]; then
     notify-send -i "$PLAYER" "$PLAYER" "starting"
     i3-msg "exec --no-startup-id $PLAYER" >/dev/null 2>&1
@@ -21,37 +17,41 @@ fi
 case $BLOCK_BUTTON in
 1)
     playerctl next "$PLAYER_ARG"
-    notify-send -i none "playerctl" "next song"
+    notify-send -i $PLAYER "playerctl" "next song"
     ;;
 2)
     playerctl play-pause "$PLAYER_ARG"
-    notify-send -i none "playerctl" "$(playerctl "$PLAYER_ARG" status 2>&1)"
+    notify-send -i $PLAYER "playerctl" "$(playerctl "$PLAYER_ARG" status 2>&1)"
     ;;
 3)
     playerctl previous "$PLAYER_ARG"
-    notify-send -i none "playerctl" "prev song"
+    notify-send -i $PLAYER "playerctl" "prev song"
     ;;
 4)
     playerctl "$PLAYER_ARG" volume "0.05+"
-    notify-send -i none "playerctl" "volume +5%"
+    notify-send -i $PLAYER "playerctl" "volume +5%"
     ;;
 5)
     playerctl "$PLAYER_ARG" volume "0.05-"
-    notify-send -i none "playerctl" "volume -5%"
+    notify-send -i $PLAYER "playerctl" "volume -5%"
     ;;
 esac
 
 case "$PLAYER_STATUS" in
-"Playing")
-    printf "喇 %s\n" "$(playerctl "$PLAYER_ARG" metadata title | cut -c -30 | iconv -c)"
-    ;;
-"No players found")
-    echo " "
-    ;;
-"Stopped")
-    echo " "
-    ;;
-"Paused")
-    printf " %s\n" "$(playerctl "$PLAYER_ARG" metadata title | cut -c -30 | iconv -c)"
-    ;;
+	"Playing")
+		icon="喇 "
+		text="$(playerctl "$PLAYER_ARG" metadata title | cut -c -30 | iconv -c)"
+		;;
+	"No players found")
+		icon=" "
+		;;
+	"Stopped")
+		icon=" "
+		;;
+	"Paused")
+		icon=" "
+		text="$(playerctl "$PLAYER_ARG" metadata title | cut -c -30 | iconv -c)"
+		;;
 esac
+
+printf "%s %s\n" "$icon" "$text"
