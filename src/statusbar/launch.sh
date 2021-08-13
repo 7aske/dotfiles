218a3fc410fe049b6c4f0953f22a73248f70df8a
Nikola Tasic <ntasic7@gmail.com>
2021-08-13 14:33:46 +0200

    statusbar/launch - added counter for today launches

--- src/statusbar/launch.sh
+++ src/statusbar/launch.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 
+[ -e "$HOME/.config/colors.sh" ] && . "$HOME/.config/colors.sh" 
+
 web_url="https://go4liftoff.com/launches"
 api_url="https://fdo.rocketlaunch.live/json/launches/next/5"
 
+today="$(date +"%b %d")"
 case $BLOCK_BUTTON in
-	1) notify-send -i rocket "Upcoming launches" "$(date +"%b %d")\n\n$(curl "$api_url" | jq -jr '.result[] | .date_str, "|", .name, "|", select(.t0!=null), "\n"' | column -t -s'|')" ;;
+	1) notify-send -i rocket "Upcoming launches" "$today\n\n$(curl "$api_url" | jq -jr '.result[] | .date_str, "|", .name, "|", select(.t0!=null), "\n"' | column -t -s'|')" ;;
 	3) xdg-open "$web_url" ;;
 esac
 
-launches=$(curl "$api_url" | jq ".count")
+launch_data="$(curl -s "$api_url")"
+launches=$(echo "$launch_data" | jq ".count")
+launches_today=0
+
+IFS=$'\n'
+for launch_date in $(echo "$launch_data" | jq -jr '.result[] | .date_str, "\n"'); do
+	if [ "$launch_date" == "$today" ]; then
+		launches_today=$((launches_today+1))
+	fi
+done
+IFS=
 
 if [ "$launches" -eq 0 ] || [ "$launches" = "null" ]; then
 	exit 0
 fi
 
-echo "異 $launches"
+color="$color7"
+extra=""
+if [ "$launches_today" -gt 0 ]; then 
+	extra=" ($launches_today)"
+	color="$color3"
+fi
+
+
+
+echo "異 $launches<span color='$color'>$extra</span>"
