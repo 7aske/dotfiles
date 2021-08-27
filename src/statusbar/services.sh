7086f78f5d4c5b71e6f554b6982fd99366c53a9a
Nikola Tasic <ntasic7@gmail.com>
2021-08-27 22:00:38 +0200

    statusbar/services - on click shows active services

--- src/statusbar/services.sh
+++ src/statusbar/services.sh
@@ -1,25 +1,30 @@
-SERVICES=""
+declare -A SERVICES
 if [ "$(systemctl is-active docker)" = "active" ]; then
-	SERVICES="$SERVICES "
+	SERVICES+=(["docker"]="")
 fi
 
 if [ "$(systemctl is-active mysqld)" = "active" ] \
 	|| [ "$(systemctl is-active mariadb)" = "active" ] \
 	|| [ "$(netstat -tln | grep -cE "(127.0.0.1|::1):3306")" -gt 0 ]; then
-	SERVICES="$SERVICES  "
+	SERVICES+=(["mysql"]="")
 fi
 
 if [ "$(systemctl is-active postgresql)" = "active" ] \
 	|| [ "$(netstat -tln | grep -cE "(127.0.0.1|::1):5432")" -gt 0 ]; then
-	SERVICES="$SERVICES  "
+	SERVICES+=(["postres"]="")
 fi
 
 if [ "$(systemctl is-active nginx)" = "active" ]; then
-	SERVICES="$SERVICES  "
+	SERVICES+=(["nginx"]="")
 fi
 
-if [ -z "$SERVICES" ]; then
+if [ "${#SERVICES[@]}" -eq 0 ]; then
 	exit 0
 fi
 
-echo "<span>$SERVICES</span>"
+case $BLOCK_BUTTON in
+	1) notify-send -a services "active services" "$(for serv in ${!SERVICES[@]}; do echo "$serv"; done)" ;;
+esac
+
+echo "<span>$(for serv in ${SERVICES[@]}; do echo -n "$serv "; done)</span>"
+
