22819335a29aafb14f764cd785a7f71c3e6cce38
Nikola Tasic <ntasic7@gmail.com>
2021-09-09 22:28:53 +0200

    statusbar/weather - reads WEATHER env variable for location

--- src/statusbar/weather.sh
+++ src/statusbar/weather.sh
@@ -2,13 +2,13 @@
 
 show_weather() {
 	#i3-msg "workspace weather"
-	curl -s http://wttr.in > /tmp/weather 2>&1
+	curl -s http://wttr.in/"$WEATHER" > /tmp/weather 2>&1
 	i3-msg "exec --no-startup-id setsid -f st -c weather_floating -e less -Srf /tmp/weather" 2>/dev/null 1>/dev/null
 }
 
 case $BLOCK_BUTTON in
-	1) notify-send -i none "Weather" "$(curl wttr.in/ | perl -pe 's/\e\[[0-9;]*m(?:\e\[K)?//g' | head -7)" ;;
+	1) notify-send -i none "Weather" "$(curl wttr.in/"$WEATHER" | perl -pe 's/\e\[[0-9;]*m(?:\e\[K)?//g' | head -7)" ;;
 	3) show_weather ;;
 esac
 
-echo "$(curl wttr.in/?format=%T%c%t | cut -c 14- | iconv -c)"
+echo "$(curl wttr.in/"$WEATHER"?format=%T%c%t | cut -c 14- | iconv -c)"
