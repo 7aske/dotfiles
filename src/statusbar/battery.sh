14e74b897bba47b576f3404fd8b0a7fc990c9974
Nikola Tasic <ntasic7@gmail.com>
2021-12-18 19:07:17 +0100

    statusbar/battery - changed colors

--- src/statusbar/battery.sh
+++ src/statusbar/battery.sh
@@ -4,7 +4,13 @@
 [ -f  "$HOME/.config/colors.sh" ] && . "$HOME/.config/colors.sh"
 [ -f  "$HOME/.cache/wal/colors.sh" ] && . "$HOME/.cache/wal/colors.sh"
 
+status=$(cat /sys/class/power_supply/"$1"/status)
+capacity=$(cat /sys/class/power_supply/"$1"/capacity) || exit
+
 case $BLOCK_BUTTON in
+	1) 
+		duration=$(acpi | awk '{print substr($5, 0, length($5) - 3)}')
+		notify-send -a battery -i battery "Battery" "$([ "$status" = "Charging" ] && printf "Until charged" || printf "Remaining"): $duration" ;;
     3) pgrep -x dunst >/dev/null && notify-send " Battery module" "\n : discharging
  : not charging
  : charging
@@ -12,31 +18,28 @@ case $BLOCK_BUTTON in
  : battery very low!" ;;
 esac
 
-capacity=$(cat /sys/class/power_supply/"$1"/capacity) || exit
-duration=$(acpi | awk '{print substr($5, 0, length($5) - 3)}')
 saver_icon="  "
-status=$(cat /sys/class/power_supply/"$1"/status)
 saver="$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)"
 
-if [ "$capacity" -ge 75 ]; then
-	color="${color7:-"#ffffff"}"
-elif [ "$capacity" -ge 50 ]; then
-    color="${color2:-"#77dd77"}"
+if [ "$capacity" -ge 65 ]; then
+	color="${color7:-"#D8DEE9"}"
+elif [ "$capacity" -ge 40 ]; then
+    color="${color3:-"#EBCB8B"}"
 elif [ "$capacity" -ge 25 ]; then
-	color="${color3:-"#ff5252"}"
+	color="${theme12:-"#D08770"}"
 else
-	color="${color1:-"#ff8144"}"
+	color="${color1:-"#BF616A"}"
 	warn="  "
 fi
 
 [ -z $warn ] && warn=" "
 [ "$saver" -eq 0 ] && saver_icon=""
 
-[ "$status" = "Charging" ] && color="#ffffff"
+[ "$status" = "Charging" ] && color="${color2:-"#A3BE8C"}"
 
 printf "<span color='%s'>%s%s%s%s</span>\n" \
 	"$color" \
 	"$(echo "$status" | sed -e "s/,//;s/Discharging//;s/Not Charging//;s/Charging//;s/Unknown//;s/Full//;s/ 0*/ /g;s/ :/ /g")" \
 	"$warn" \
-	"$(echo "$capacity" | sed -e 's/$/%/') $([ -n "$duration" ] && echo "($duration)")" \
+	"$(echo "$capacity" | sed -e 's/$/%/')" \
 	"$saver_icon"
