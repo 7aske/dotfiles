80c833b644f9bdc19d0003cbb2b48e244823550a
Nikola Tasic <ntasic7@gmail.com>
2022-01-11 12:12:31 +0100

    statusbar/services - fixed port matching regex; increased icon size

--- src/statusbar/services.sh
+++ src/statusbar/services.sh
@@ -5,12 +5,12 @@ fi
 
 if [ "$(systemctl is-active mysqld)" = "active" ] \
 	|| [ "$(systemctl is-active mariadb)" = "active" ] \
-	|| [ "$(netstat -tln | grep -cE "(127.0.0.1|::1):3306")" -gt 0 ]; then
+	|| [ "$(netstat -tln | grep -cE "([0-9]+.[0-9]+.[0-9]+.[0-9]+|::[1:]):3306\b")" -gt 0 ]; then
 	SERVICES+=(["mysql"]="")
 fi
 
 if [ "$(systemctl is-active postgresql)" = "active" ] \
-	|| [ "$(netstat -tln | grep -cE "(127.0.0.1|::1):5432")" -gt 0 ]; then
+	|| [ "$(netstat -tln | grep -cE "([0-9]+.[0-9]+.[0-9]+.[0-9]+|::[1:]):5432\b")" -gt 0 ]; then
 	SERVICES+=(["postres"]="")
 fi
 
@@ -26,5 +26,5 @@ case $BLOCK_BUTTON in
 	1) notify-send -a services "active services" "$(for serv in ${!SERVICES[@]}; do echo "$serv"; done)" ;;
 esac
 
-echo "<span> $(for serv in ${SERVICES[@]}; do echo -n "$serv "; done)</span>"
+echo "<span size='x-large'> $(for serv in ${SERVICES[@]}; do echo -n "$serv "; done)</span>"
 
