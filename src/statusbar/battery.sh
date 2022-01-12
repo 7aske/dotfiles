f6199b082e42ee612f61e07687ceb38924c3099a
Nikola Tasic <ntasic7@gmail.com>
2022-01-13 00:58:02 +0100

    statusbar/battery - changing battery icon depending on capacity

--- src/statusbar/battery.sh
+++ src/statusbar/battery.sh
@@ -10,37 +10,63 @@ status=$(cat /sys/class/power_supply/"$1"/status)
 capacity=$(cat /sys/class/power_supply/"$1"/capacity) || exit
 
 case $BLOCK_BUTTON in
-	1) 
-		duration=$(acpi | awk '{print substr($5, 0, length($5) - 3)}')
+	1) duration=$(acpi | awk '{print substr($5, 0, length($5) - 3)}')
 		notify-send -a battery -i battery "Battery" "$([ "$status" = "Charging" ] && printf "Until charged" || printf "Remaining"): $duration" ;;
 	2) [ -e "$SWITCH" ] && rm "$SWITCH" || touch "$SWITCH" ;;
-    3) pgrep -x dunst >/dev/null && notify-send " Battery module" "\n : discharging
+    3) notify-send " Battery module" "\n : discharging
  : not charging
  : charging
 : charged/stagnant charge
  : battery very low!" ;;
 esac
 
-saver_icon="  "
 warn=" "
-saver="$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)"
 
-if [ "$capacity" -ge 65 ]; then
+if [ "$capacity" -eq 100 ]; then
 	color="${color7:-"#D8DEE9"}"
-elif [ "$capacity" -ge 40 ]; then
+	icon=""
+elif [ "$capacity" -ge 90 ]; then
+	color="${color7:-"#D8DEE9"}"
+	icon=""
+elif [ "$capacity" -ge 80 ]; then
+	color="${color7:-"#D8DEE9"}"
+	icon=""
+elif [ "$capacity" -ge 70 ]; then
+	color="${color7:-"#D8DEE9"}"
+	icon=""
+elif [ "$capacity" -ge 60 ]; then
     color="${color3:-"#EBCB8B"}"
-elif [ "$capacity" -ge 25 ]; then
+	icon=""
+elif [ "$capacity" -ge 50 ]; then
+    color="${color3:-"#EBCB8B"}"
+	icon=""
+elif [ "$capacity" -ge 40 ]; then
 	color="${theme12:-"#D08770"}"
+	icon=""
+elif [ "$capacity" -ge 30 ]; then
+	color="${theme12:-"#D08770"}"
+	icon=""
+elif [ "$capacity" -ge 20 ]; then
+	color="${color1:-"#BF616A"}"
+	icon=""
+	warn="  "
+elif [ "$capacity" -ge 10 ]; then
+	color="${color1:-"#BF616A"}"
+	icon=""
+	warn="  "
 else
 	color="${color1:-"#BF616A"}"
+	icon=""
 	warn="  "
 fi
 
 [ -z $warn ] && warn=" "
-[ "$saver" -eq 0 ] && saver_icon=""
 
 [ "$status" = "Charging" ] && color="${color2:-"#A3BE8C"}"
-icon="$(echo "$status" | sed -e "s/,//;s/Discharging//;s/Not Charging//;s/Charging//;s/Unknown//;s/Full//;s/ 0*/ /g;s/ :/ /g")"
+if ! [ "$status" = "Discharging" ]; then
+	icon="$(echo "$status" | sed -e "s/,//;s/Discharging//;s/Not Charging//;s/Charging//;s/Unknown//;s/Full//;s/ 0*/ /g;s/ :/ /g")"
+fi
+
 
 if [ -e "$SWITCH" ]; then
 	echo "<span color='$color'>$icon$warn</span>"
