920b29a6ff50118218ef7f8c441366e122f802f7
Nikola Tasic <ntasic7@gmail.com>
2024-08-07 11:16:02 +0200

    nvim - disabled global autoformat

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,11 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.colorcolumn = "80"
-opt.scrolloff = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 2
 
 vim.b.autoformat = false
+vim.g.autoformat = false
