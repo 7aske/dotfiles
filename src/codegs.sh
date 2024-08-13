b86dd88960e5421dfecdc3945b45d3c1b7090e31
Nikola Tasic <ntasic7@gmail.com>
2024-08-13 10:14:10 +0200

    codegs - preview

--- src/codegs.sh
+++ src/codegs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 
-echo $(/usr/bin/cgs -d | fzf --reverse --cycle)
+echo $(/usr/bin/cgs -d | fzf --preview 'git -c color.status=always -C {} status' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --reverse --cycle)
