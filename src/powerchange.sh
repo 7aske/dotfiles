9b8f6e6469c5270ab76617100c4e73ee0bc5a398
Nikola Tasic <ntasic7@gmail.com>
2024-08-24 10:30:10 +0200

    powerchange - fixed systemctl check

--- src/powerchange.sh
+++ src/powerchange.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-systemctl is-active acpid || echo "acpid not active" && exit 0
+if ! ( systemctl is-active acpid );then
+    echo "acpid is not running"
+    exit 0
+fi
 
 umask 000                  # allow all users to access the file we're about to create
 exec 9>"/tmp/${0##*/}.lck" # open lockfile on FD 9, based on basename of argv[0]
 umask 022                  # move back to more restrictive file permissions
-flock -x -n 9 || echo "powerchange already active" && exit 0     # grab that lock, or exit the script early
+flock -x -n 9 || exit 0    # grab that lock, or exit the script early
 
 acpi_listen | while IFS= read -r line; do
     if [[ "$line" =~ ^ac_adapter.*$ ]]; then
