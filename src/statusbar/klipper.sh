e139b9ae0360372ef368ac893d15a58148f52e41
Nikola Tasic <ntasic7@gmail.com>
2025-02-19 22:50:01 +0100

    klipper stuff

--- src/statusbar/klipper.sh
+++ src/statusbar/klipper.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$KLIPPER_HOST" ]; then 
-    echo "KLIPPER_HOST is not set"
-    exit 0
-fi
-
 SWITCH="$HOME/.cache/statusbar_$(basename $0)"
 
 while getopts "j" opt; do
@@ -13,53 +8,92 @@ while getopts "j" opt; do
     esac
 done
 
-text_icon_normal="󰹛"
-text_icon_printing="󱇀"
-json_icon_normal="3d_printer"
-json_icon_printing="3d_printer_printing"
+declare -A json_icons
+json_icons[printing]="3d_printer_printing"
+json_icons[paused]="3d_printer"
+json_icons[standby]="3d_printer"
+json_icons[complete]="3d_printer"
+json_icons[error]="3d_printer"
+json_icons[canceled]="3d_printer"
+declare -A icons
+icons[printing]="󱇀"
+icons[paused]="󰏤"
+icons[standby]="󰐫"
+icons[complete]=""
+icons[error]=""
+icons[canceled]="󰜺"
+declare -A colors
+colors[printing]="Warning"
+colors[paused]="Warning"
+colors[standby]="Idle"
+colors[complete]="Good"
+colors[error]="Critical"
+colors[canceled]="Idle"
 
-text_icon="$text_icon_printing"
-json_icon="$json_icon_printing"
+_output() {
+    local color="${colors[$1]}"
+    local text="$2"
+    local icon
+    if [ "$json" = "true" ]; then
+        icon="${json_icons[$1]}"
+        echo '{"icon": "'$icon'", "state":"'${color:-"Idle"}'", "text":"'$text'"}';
+    else
+        icon="${icons[$1]}"
+        echo "$icon $text"
+    fi
+}
+
+_format_time() {
+    local formula="$1"
+    local suffix="$2"
+    local val="$(bc <<< "$formula")"
+
+    if [ "$val" -eq 0 ]; then
+        echo ""
+    else
+        echo "${val}${suffix}"
+    fi
+}
+
+if [ -z "$KLIPPER_HOST" ]; then 
+    _output "error" ""
+    exit 0
+fi
+
 
 progress=$(curl -s $KLIPPER_HOST/printer/objects/query?display_status | jq -r '.result.status.display_status.progress')
-print_stats=$(curl -s $KLIPPER_HOST/printer/objects/query?print_stats)
-percent="$(echo "$progress * 100" | bc)"
-print_duration=$(echo "$print_stats" | jq -r '.result.status.print_stats.print_duration')
-remaining_duration="$(echo "($print_duration/$progress-$print_duration)" | bc)"
-filename="$(echo "$print_stats" | jq -r '.result.status.print_stats.filename')"
-status="$(echo "$print_stats" | jq -r '.result.status.print_stats.state')"
+print_stats=$(curl -s $KLIPPER_HOST/printer/objects/query?print_stats | jq -r '.result.status.print_stats')
 
-remaining_hours="$(echo "scale=0;$remaining_duration/3600" | bc)"
-remaining_minutes="$(echo "scale=0;($remaining_duration%3600)/60" | bc)"
-print_hours="$(echo "scale=0;$print_duration/3600" | bc)"
-print_minutes="$(echo "scale=0;(${print_duration}%3600)/60" | bc)"
+print_duration="$(jq -r '.print_duration' <<< "$print_stats")"
+filename="$(jq -r '.filename' <<< "$print_stats")"
+status="$(jq -r '.state' <<< "$print_stats")"
 
-if [ "$remaining_hours" -eq 0  ]; then
-    remaining_hours=""
-else
-    remaining_hours="${remaining_hours}h"
-fi
-
-if [ "$remaining_minutes" -eq 0 ]; then
-    remaining_minutes=""
-else
-    remaining_minutes="${remaining_minutes}m"
-fi
+percent="$(bc <<< "$progress * 100")"
+remaining_duration="$(bc <<< "($print_duration/$progress-$print_duration)")"
+remaining_hours="$(_format_time "scale=0;$remaining_duration/3600" "h")"
+remaining_minutes="$(_format_time "scale=0;($remaining_duration%3600)/60" "m")"
+print_hours="$(_format_time "scale=0;$print_duration/3600" "h")"
+print_minutes="$(_format_time "scale=0;(${print_duration}%3600)/60" "m")"
 
 _notify_progress() {
-    thumbnail="$(curl -s \
-        "$KLIPPER_HOST/server/files/thumbnails?filename=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$filename")" | jq -r '.result | max_by(.width) | .thumbnail_path')"
-            thumbnail="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$thumbnail")"
-            curl -s "$KLIPPER_HOST/server/files/gcodes/$thumbnail" > "/tmp/$thumbnail"
-            answ="$(notify-send -i "/tmp/$thumbnail" \
-                -A "open=Open klipper"  \
-                -h "int:value:$percent" \
-                "3D Printer" "File: $filename\n\nDuration: ${print_hours}h ${print_minutes}m\nRemaining: ${remaining_hours} ${remaining_minutes}\nProgress: ${percent%.*}%")"
-            case "$answ" in
-                "open")
-                    xdg-open $KLIPPER_HOST
-                    ;;
-            esac
+    local escaped_filename="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$filename")"
+    local thumbnail="$(curl -s "$KLIPPER_HOST/server/files/thumbnails?filename=$escaped_filename" \
+        | jq -r '.result | max_by(.width) | .thumbnail_path' \
+        | xargs perl -MURI::Escape -e 'print uri_escape($ARGV[0]);')"
+    [ -e "$thumbnail" ] || curl -s "$KLIPPER_HOST/server/files/gcodes/$thumbnail" > "/tmp/$thumbnail"
+    local body="$(cat << EOF
+Duration: ${print_hours} ${print_minutes}
+Remaining: ${remaining_hours} ${remaining_minutes}
+Progress: ${percent%.*}% 
+EOF
+)"
+    local answ="$(notify-send -i "/tmp/$thumbnail" \
+        -A "open=Open klipper"  \
+        -h "int:value:$percent" \
+        "$filename" "$body")"
+    case "$answ" in
+        "open") xdg-open "$KLIPPER_HOST" ;;
+    esac
 }
 
 last_percentage="$([ -e "/tmp/$filename" ] && cat "/tmp/$filename" || echo 0)"
@@ -67,13 +101,12 @@ if ! [[ "$last_percentage" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
     last_percentage=0
 fi
 
-if [ "$(printf "%0.f > (%.0f + 10)\n" "$percent" "$last_percentage" | bc -l)" -eq "1" ] && [ "$status" = "printing" ]; then
+if [ "$(printf "%0.f > (%.0f + 5)\n" "$percent" "$last_percentage" | bc -l)" -eq "1" ] && [ "$status" = "printing" ]; then
     echo "$percent" > "/tmp/$filename"
     if [ -z "$BLOCK_BUTTON" ]; then
         _notify_progress &
     fi
 fi
-
 
 case $BLOCK_BUTTON in 
     1) xdg-open $KLIPPER_HOST ;;
@@ -81,28 +114,15 @@ case $BLOCK_BUTTON in
     3) _notify_progress &;;
 esac
 
-_json() {
-    echo '{"icon": "'${1}'", "state":"'${2:-"Idle"}'", "text":"'${3}'"}';
-}
-
 if [ "$status" != "printing" ]; then
-    if [ -n "$json" ]; then
-        _json "$json_icon" "Good" ""
-    else
-        text=""
-    fi
+    _output "$status" "${status,}"
     exit 0
 fi
 
-
 if [ -e "$SWITCH" ]; then
-    text="$(printf "%d%% %s%s" "$percent" "$remaining_hours" "$remaining_minutes")"
+    text="$(printf "%.0f%% %s%s" "$percent" "$remaining_hours" "$remaining_minutes")"
 else
-    text="$(printf "%d%%" "$percent")"
+    text="$(printf "%.0f%%" "$percent")"
 fi
 
-if [ -n "$json" ]; then
-    _json "$json_icon" "Warning" "$text " 
-else
-    echo "$text_icon $text"
-fi
+_output "$status" "$text"
