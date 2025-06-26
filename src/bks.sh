fd8d69dc6b6ce11f41a06b2de1261ed1bda0d91d
Nikola Tasic <ntasic7@gmail.com>
2025-06-26 20:09:15 +0200

    bks - profile specific

--- src/bks.sh
+++ src/bks.sh
@@ -2,13 +2,25 @@
 
 BROWSER_FLAGS="${BROWSER_FLAGS:-"--new-window"}"
 
+
+_brave_bks() {
+    local flags="$XDG_CONFIG_HOME/brave-flags.conf"
+
+    local profile="Default"
+
+    if [ -e "$flags" ]; then
+        profile="$(cat "$flags" | grep -E '^--profile-directory=' | cut -d'=' -f2)"
+    fi
+
+    SELECTED_URL="$(cat "$XDG_CONFIG_HOME/BraveSoftware/Brave-Browser/$profile/Bookmarks" \
+        | jq -r '.roots.bookmark_bar.children[] | if .children then (.children[] | select(.url) ) else . end | (.name + " (" + (.url) + ")")' \
+        | rofi -i -dmenu \
+        | sed -e 's/^.* (\(.*\))$/\1/')"
+}
+
 case "$BROWSER" in
     "brave-browser" | "brave")
-        SELECTED_URL="$(cat "$XDG_CONFIG_HOME/BraveSoftware/Brave-Browser/Default/Bookmarks" \
-            | jq -r '.roots.bookmark_bar.children[] | if .children then (.children[] | select(.url) ) else . end | (.name + " (" + (.url) + ")")' \
-            | rofi -i -dmenu \
-            | sed -e 's/^.* (\(.*\))$/\1/')"
-        ;;
+        _brave_bks ;;
     *) echo "$BROWSER is not supported."
         exit 1
         ;;
