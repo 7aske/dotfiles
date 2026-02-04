2d9f5e0c37ffd17b5a6e425782ffd4824a6befab
Nikola Tasic <ntasic7@gmail.com>
2026-02-04 01:59:05 +0100

    screenlayout - names shown without extensions

--- src/screenlayout.sh
+++ src/screenlayout.sh
@@ -5,10 +5,13 @@ SCREENLAYOUT_DIR="$HOME/.screenlayout"
 if [ -n "$1" ]; then
     LAYOUT="$1"
 else
-    LAYOUT="$(basename "$(find "$SCREENLAYOUT_DIR" -type f -name "*.sh" -printf "%f\n" | rofi -dmenu -p "Select screen layout")")"
+    LAYOUT="$(basename "$(find "$SCREENLAYOUT_DIR" -type f -name "*.sh" -printf "%f\n" | cut -d '.' -f1 | rofi -dmenu -p "Select screen layout")")"
 fi
 
-if [ -n "$LAYOUT" ]; then
-    notify-send -a screenlayout -i display -t 1000 "Screenlayout" "Applying screen layout: $LAYOUT"
-    bash "$SCREENLAYOUT_DIR/$LAYOUT"
+if [ -z "$LAYOUT" ]; then
+    echo "No layout selected"
+    exit 1
 fi
+
+notify-send -a screenlayout -i display -t 1000 "Screenlayout" "Applying screen layout: $LAYOUT"
+bash "$SCREENLAYOUT_DIR/$LAYOUT.sh"
