66f9eeba566f4184e7798c3adf1f5c8dfb601136
Nikola Tasic <ntasic7@gmail.com>
2026-05-01 01:47:18 +0200

    statusbar/layout - improvements to formatting

--- src/statusbar/layout.sh
+++ src/statusbar/layout.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+declare -A kb_mapping=(
+    ["us (N/A)"]="EN"
+    ["rs (latin)"]="SR"
+    ["rs (N/A)"]="ЋИР"
+)
 
 case $BLOCK_BUTTON in
 	1) notify-send -i keyboard "Keyboard Layout" "Toggle" && kblang -l us,rs-latin,rs -t ;;
@@ -6,4 +12,24 @@ case $BLOCK_BUTTON in
 esac
 
 
-setxkbmap -query | grep layout | awk '{print $2}'
+layout="$(setxkbmap -query | awk '
+BEGIN { layout=""; variant="" }
+{
+    if ($1 == "layout:") {
+        layout=$2
+    }
+    if ($1 == "variant:") {
+        variant=$2
+    }
+}
+END { 
+    if (layout && variant) {
+        print layout " (" variant ")"
+    } else if (layout) {
+        print layout " (N/A)"
+    }
+}')"
+
+display="${kb_mapping["$layout"]}"
+
+echo "${display:-$layout}"
