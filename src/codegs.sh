c8f2415f4a1a6b3052ece25387b3946edaaf5b69
Nikola Tasic <ntasic7@gmail.com>
2026-05-03 18:38:54 +0200

    codegs - ctrl+g opens lazygit

--- src/codegs.sh
+++ src/codegs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 
-echo $(/usr/bin/cgs -d | fzf --preview 'git -c color.status=always -C {} status' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --reverse --cycle)
+echo $(/usr/bin/cgs -d | fzf --preview 'git -c color.status=always -C {} status' --bind 'ctrl-g:execute(cd {} && lazygit)' --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' --reverse --cycle)
