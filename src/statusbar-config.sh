e2272df889b73fd86164c78710da2b95248c1ed1
Nikola Tasic <ntasic7@gmail.com>
2026-05-03 18:22:04 +0200

    statusbar-config -  show kill file status in prompt

--- src/statusbar-config.sh
+++ src/statusbar-config.sh
@@ -6,13 +6,17 @@ STATUSBAR_FILE="$HOME/.cache/statusbar"
 
 module="$(for m in $(dir -1 "$STATUSBAR_DIR"); do
     if grep -E 'KILL_SWITCH|libbar_kill_switch' "$STATUSBAR_DIR/$m" &> /dev/null; then
-        echo "$m"
+        if [ "$m" = "libbar" ]; then
+            continue
+        fi
+        KILL_FILE="$STATUSBAR_FILE"_"$m"_kill
+        echo "($([ -e "$KILL_FILE" ] && echo "x" || echo "✓")) $m"
     fi
 done | rofi -dmenu -i -p "Toggle statusbar module")"
 
 
 if [ -n "$module" ]; then
-    KILL_FILE="$STATUSBAR_FILE"_"$module"_kill
+    KILL_FILE="$STATUSBAR_FILE"_"$(echo "$module" | sed -n 's/([x|✓]) \(.*\)/\1/p')"_kill
 
     if [ -e "$KILL_FILE" ]; then
         rm "$KILL_FILE"
