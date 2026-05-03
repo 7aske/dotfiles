f35dde63e1dc007ea3e0eb2cd411ca69c624958f
Nikola Tasic <ntasic7@gmail.com>
2026-05-03 17:58:52 +0200

    statusbar/memory - open htop memory sorted

--- src/statusbar/memory.sh
+++ src/statusbar/memory.sh
@@ -5,7 +5,7 @@ SWITCH="$HOME/.cache/statusbar_$(basename $0)"
 case $BLOCK_BUTTON in
 	1) notify-send -i device_mem "Memory hogs" "$(smem -Hkar | head)" ;;
 	2) [ -e "$SWITCH" ] && rm "$SWITCH" || touch "$SWITCH" ;;
-	3) wtoggle2 -T htop ;;
+	3) wtoggle2 -T htop --sort-key PERCENT_MEM ;;
 esac
 
 declare -a FREE
