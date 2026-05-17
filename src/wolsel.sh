d59562d725ec64fe53f92ca21401f0b6eadae221
Nikola Tasic <ntasic7@gmail.com>
2026-05-18 00:50:17 +0200

    wolsel - now fetches device info from dns/dhcp server

--- src/wolsel.sh
+++ src/wolsel.sh
@@ -1,44 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-prog="$(basename $0)"
+DNS_SERVER="pioneer.local"
 
-hosts="$HOME/.local/etc/hosts"
-
-[ ! -e "$hosts" ] && echo "$prog: $hosts: no such file or directory" && exit 1
-
-
-mapfile -t options < <(sed -e '/^#.*/d' -e '/^[[:space:]]*$/d' "$hosts")
-
-if [ "$?" -eq 1 ]; then
-    echo "$prog: could not read hosts file"
-    exit 1
+menu="rofi -dmenu"
+if [ -t 1 ]; then
+    menu="fzf"
 fi
 
-for i in "${!options[@]}"; do 
-    host="$(echo "${options[$i]}" | awk '{print $1}')"
-    ip="$(echo "${options[$i]}" | awk '{print $2}')"
-    mac="$(echo "${options[$i]}" | awk '{print $3}')"
-    printf "%d) %-10s %10s %10s\n" "$((i+1))" "$host" "$ip" "$mac"
-done
-
-declare reply
-
-if [ -n "$1" ]; then
-    reply=$1
-else
-    printf "%s" "Select the device: "
-    read -r reply
-fi
-
-reply=$((reply-1))
-
-if [ "$reply" -lt $(("${#options[@]}"+1)) ]; then
-    host="$(echo "${options[$reply]}" | awk '{print $1}')"
-    ip="$(echo "${options[$reply]}" | awk '{print $2}')"
-    mac="$(echo "${options[$reply]}" | awk '{print $3}')"
-    wakeonlan "$mac" &> /dev/null
-    printf "Waking up %s@%s\n" "$host" "$ip"
-else
-    echo "Invalid index"
-fi
-
+ssh "$DNS_SERVER" "rg --only-matching --no-line-number '\w{2}(:\w\w){5},\d{1,3}(\.\d{1,3}){3},.*,infinite' infra/pihole/etc-pihole/pihole.toml" |
+    awk -F, '{print $3 " " $1 " " $2}' | \
+    $menu | \
+    awk '{print $2}' | \
+    xargs -r -I% ssh "$DNS_SERVER" 'wakeonlan % '
