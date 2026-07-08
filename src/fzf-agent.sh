5d5e7a60a56bbdb383e91f09327747bb2fd957f5
Nikola Tasic <ntasic7@gmail.com>
2026-07-08 17:28:00 +0200

    setting window role for tmux sessions

--- src/fzf-agent.sh
+++ src/fzf-agent.sh
@@ -4,8 +4,11 @@
 
 sessions="$({ tmux ls -F#S; echo 'agent-default'; } | grep --color=none 'agent-' | sort | uniq)"
 
+
+if [ -n "$1" ]; then
+    session="$1"
 # if only one session is found, attach to it
-if [ "$(echo "$sessions" | wc -l)" -eq 1 ]; then
+elif [ "$(echo "$sessions" | wc -l)" -eq 1 ]; then
     session="$sessions"
 else
     session="$(echo "$sessions" | fzf --prompt 'Agent session: ')"
@@ -17,6 +20,10 @@ fi
 
 if ! tmux has-session -t "$session" 2>/dev/null; then
     tmux new -s "$session" -d -c "$HOME/.agent" "$AGENT"
+fi
+
+if [ -n "$WINDOWID" ]; then
+    xprop -id "$WINDOWID" -f WM_WINDOW_ROLE 8s -set WM_WINDOW_ROLE "tmux-$session"
 fi
 
 tmux a -t "$session"
