9eb54799910e275116a20e460938db9111332e4f
Nikola Tasic <ntasic7@gmail.com>
2026-07-20 11:53:55 +0200

    ranger - mime type popup

--- config/ranger/commands.py
+++ config/ranger/commands.py
@@ -1,4 +1,5 @@
 from ranger.api.commands import Command
+import os
 import subprocess
 
 class rgs_cd(Command):
@@ -10,3 +11,25 @@ class rgs_cd(Command):
             return
         if path:
             self.fm.cd(path)
+
+
+class mime_ext(Command):
+    def execute(self):
+        thisfile = self.fm.thisfile
+        if thisfile is None:
+            self.fm.notify("No file selected", bad=True)
+            return
+
+        path = thisfile.path
+        extension = os.path.splitext(path)[1].lstrip(".") or "[none]"
+
+        try:
+            mime_type = subprocess.check_output(
+                ["xdg-mime", "query", "filetype", path],
+                text=True
+            ).strip()
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            self.fm.notify("xdg-mime query failed", bad=True)
+            return
+
+        self.fm.notify(f"mime: {mime_type} | ext: {extension}")
