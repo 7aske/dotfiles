362573813d1e03c776abc803f6c706d6b2127722
Nikola Tasic <ntasic7@gmail.com>
2026-08-19 09:01:39 +0200

    screenlayout - screenlayout icons

--- src/screenlayout.sh
+++ src/screenlayout.sh
@@ -1,11 +1,56 @@
 #!/usr/bin/env bash
 
 SCREENLAYOUT_DIR="$HOME/.screenlayout"
+DEFAULT_LAYOUT_ICON="󰍺"
+
+get_layout_icon() {
+    local layout_path="$1"
+    local icon
+
+    # Layout scripts can define either:
+    #   SCREENLAYOUT_ICON="󰡘"
+    # or:
+    #   # icon: 󰡘
+    icon="$(sed -nE '
+        s/^[[:space:]]*SCREENLAYOUT_ICON[[:space:]]*=[[:space:]]*["'"'"']?([^"'"'"']+)["'"'"']?[[:space:]]*$/\1/p
+        t found
+        s/^[[:space:]]*#[[:space:]]*icon[[:space:]]*:[[:space:]]*(.+)[[:space:]]*$/\1/p
+        t found
+        b
+        :found
+        q
+    ' "$layout_path")"
+
+    if [ -n "$icon" ]; then
+        echo "$icon"
+    else
+        echo "$DEFAULT_LAYOUT_ICON"
+    fi
+}
 
 if [ -n "$1" ]; then
     LAYOUT="$1"
 else
-    LAYOUT="$(basename "$(find "$SCREENLAYOUT_DIR" -type f -name "*.sh" -printf "%f\n" | cut -d '.' -f1 | rofi -dmenu -p "Select screen layout")")"
+    if [ ! -d "$SCREENLAYOUT_DIR" ]; then
+        echo "Layout directory not found: $SCREENLAYOUT_DIR"
+        exit 1
+    fi
+
+    readarray -t layouts < <(find "$SCREENLAYOUT_DIR" -type f -name "*.sh" -printf "%f\n" | sed 's/\.sh$//' | sort)
+
+    if [ "${#layouts[@]}" -eq 0 ]; then
+        echo "No layouts found in $SCREENLAYOUT_DIR"
+        exit 1
+    fi
+
+    menu_entries=()
+    for layout in "${layouts[@]}"; do
+        layout_path="$SCREENLAYOUT_DIR/$layout.sh"
+        menu_entries+=("$(get_layout_icon "$layout_path")"$'\t'"$layout")
+    done
+
+    selected="$(printf '%s\n' "${menu_entries[@]}" | rofi -dmenu -p "Select screen layout")"
+    LAYOUT="${selected#*$'\t'}"
 fi
 
 if [ -z "$LAYOUT" ]; then
